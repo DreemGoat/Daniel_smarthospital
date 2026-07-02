a44ca09516b72a778a200f1d4db8f89cfb8df4c2
DreemGoat <91465902+DreemGoat@users.noreply.github.com>
2026-07-02 14:23:51 +0700

    Update hospital_app.py

--- hospital_app.py
+++ hospital_app.py
@@ -73,3 +73,42 @@ st.markdown("""
             Smart Hospital Patient Navigator
 </div>
 """, unsafe_allow_html=True)
+#when the form is submitted
+if submitted:
+  patient = pd,DataFrame([{
+    'age'              :age,
+    'gender'           :gender_map.get(gender,0),
+    'fever'            :int(fever),
+    'cough'            :int(cough),
+    'headache'         :int(headache),
+    'chest_pain'       :int(chest_pain),
+    'stomach_pain'     :int(stomach_pain),
+    'shortness_breath' :int(shortness_breath),
+    'nausea_vomitting' :int(nausea_vomitting),
+    'dizziness'        :int(dizziness),
+    'skin_rash'        :int(skin_rash),
+    'temperature_level':temp_map.get(temperature_level, 1),
+    'heart_rate_level' :hr_map.get(heart_rate_level, 1),
+    'duration'         :dur_map.get(duration,1),
+    'asthma'           :int(asthma),
+    'hypertension'     :int(hypertension),
+    'heart_disease'    :int(heart_disease),
+    'chief_complaint'  :cc_map.get(chief_complaint, 9)
+  }])
+  
+  patient_scaled = patient.copy()
+  patient_scaled[cols_to_scale] = scaler.transform(patient[cols_to_scale])
+
+  pred       = model.predict(patient_scaled[features])[0]
+  proba      = model.predict_proba(patient_scaled[features])[0]
+  dept_name  = dept_map_inv[pred]
+  confidence = proba[pred] * 100
+  info       = DEPT_INFO[dept_name]
+  st.markdown(f"""
+  <div style="background:{info['bg']};border:1.6px solid {info['border']};
+              border-radius:16px;padding:28px 32px;">
+      <div style = "font-size:44px">{info['icon']}</div>
+      <div>{dept_name}</div>
+      <div>Confidence: {confidence:.1f}%</div>
+  """,unsafe_allow_html=True)
+  
